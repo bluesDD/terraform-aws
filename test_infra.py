7877ee380e008357e134ade084f30917476eaff3
bluesDD <dd11blues@gmail.com>
2019-10-17 00:43:05 +0900

    :memo: Added some words

--- test_infra.py
+++ test_infra.py
@@ -1,3 +1,5 @@
+
+# User
 def test_user_moriha(host):
     user = host.user("moriha")
     assert "moriha" == user.name
@@ -5,27 +7,29 @@ def test_user_moriha(host):
     assert 1001 == user.gid
     assert "moriha" == user.group
 
-def test_port_listens(host):
-    assert host.socket("tcp://80").is_listening
-
+# Package
 def test_apache_is_installed(host):
     apache = host.package("httpd")
     assert apache.is_installed
     assert apache.version.startswith("2.4.39")
 
+# System
 def test_systeminfo(host):
     assert "linux" == host.system_info.type
     assert "amzn" == host.system_info.distribution
 
+# Mountpoint
 def test_mountpoint(host):
     assert host.mount_point("/").exists
     assert "/dev/xvda1" == host.mount_point("/").device
 
+# Service
 def test_apahce_running_and_enabled(host):
     apache = host.service("httpd")
     assert apache.is_running
     assert apache.is_enabled
 
+# Directory
 def test_directory_httpd_exists(host):
     apache = host.file("/var/lib/httpd")
     assert apache.exists
